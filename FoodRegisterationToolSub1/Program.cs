62f25e06bafdd9d6e13b41d8fe40ec0a9b33dd32
arjan <97673925+AreArjane@users.noreply.github.com>
2024-11-06 21:25:44 +0100

    Configurate Database with Neon Postgres
    Create an enviroment variabel hold sensitiv information related to the database connection.
    
    We add the enviroment variabel to the application.json file and configurated in the program.cs.
    
    To transfer the database models to the Postgres Configuration.
    
    builder.Services.AddDbContext<ApplicationDbContext>(options =>
        options.UseNpgsql(connectionString));
    Console.WriteLine("Connection String: " + connectionString);
    
    In order to run :
    
     dotnet-ef migrations add InitialCreate --context ApplicationDbContext
    
    dotnet -ef database update

--- FoodRegisterationToolSub1/Program.cs
+++ FoodRegisterationToolSub1/Program.cs
@@ -1,4 +1,26 @@
+using Microsoft.EntityFrameworkCore;
+using FoodRegistrationToolSub1.Models.datasets;
+using DotNetEnv;
+
+Env.Load();
+
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Load environment variables
+var dbHost = Env.GetString("DB_HOST") ?? "localhost";
+var dbPort = Env.GetString("DB_PORT") ?? "5432";
+var dbUser = Env.GetString("DB_USER") ?? "your_default_user";
+var dbPassword = Env.GetString("DB_PASSWORD") ?? "your_default_password";
+var dbName =Env.GetString("DB_NAME") ?? "neondb";
+
+// Build the connection string
+var connectionString = $"Host={dbHost};Port={dbPort};Username={dbUser};Password={dbPassword};Database={dbName}";
+
+// Register ApplicationDbContext with the DI container
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseNpgsql(connectionString));
+Console.WriteLine("Connection String: " + connectionString);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -9,7 +31,6 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
