0789b963529d47ea4cf892571d153f17d54bdb3e
armah9337 <97673925+AreArjane@users.noreply.github.com>
2024-11-08 02:59:40 +0100

    Update AdminCOntroller
    Add admin retriving all user with pagination and filter Firstname letters

--- FoodRegisterationToolSub1/Controllers/UsersController/AdminUserController.cs
+++ FoodRegisterationToolSub1/Controllers/UsersController/AdminUserController.cs
@@ -1,2 +1,63 @@
 //Admin Controller with Pagination impovment
 
+using FoodRegisterationToolSub1.Models.users;
+using Microsoft.AspNetCore.Mvc;
+
+[Route("api/[controller]")]
+[ApiController]
+
+public class AdminUserController : ControllerBase { 
+
+    private readonly ApplicationDbContext _context;
+
+    public AdminUserController(ApplicationDbContext context) { 
+        _context = context;
+    }
+
+    [HttpGet("adm/allNormalUser")]
+
+    public IActionResult GetAllNormalUser(
+
+        //<<required>> access controll
+
+        int pageNumber = 1, 
+        int pageSize = 20, 
+        string NuserStartWith = null) {
+
+            var userQ = _context.NormalUsers.AsQueryable();
+
+            if(!string.IsNullOrEmpty(NuserStartWith)) { 
+
+                userQ = userQ.Where(nu => nu.FirstName.StartsWith(NuserStartWith));
+            }
+
+            var totalUsers = userQ.Count();
+            var totalPage = (int)Math.Ceiling(totalUsers / (double)pageSize);
+
+            var normal_user = userQ 
+            .Skip((pageNumber - 1) * pageSize).Take(pageSize).Select(nu => new {
+
+                nu.FirstName,
+                nu.PhoneNr,
+                nu.LastName,
+                nu.Email,
+                nu.HomeAddress,
+                nu.PostalCode
+
+            }).ToList();
+
+            var response = new {
+                TotalCount = totalUsers,
+                TotalPages = totalPage,
+                CurrentPage = pageNumber,
+                PageSize = pageSize,
+                Data = normal_user
+            };
+
+            return Ok(response);
+
+
+        }
+
+
+}
