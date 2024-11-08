fa74501fc3646eca4458be2e3a9f3db39ebeeb18
armah9337 <97673925+AreArjane@users.noreply.github.com>
2024-11-08 02:03:02 +0100

    Manage directory structure

--- FoodRegisterationToolSub1/obj/Debug/net8.0/FoodRegisterationToolSub1.AssemblyInfo.cs
+++ FoodRegisterationToolSub1/obj/Debug/net8.0/FoodRegisterationToolSub1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FoodRegisterationToolSub1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+930f5e2327539803457a83ae9e292439bbb65e3f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe4881d71648c0228486ce384ea2c2beb286b53a")]
 [assembly: System.Reflection.AssemblyProductAttribute("FoodRegisterationToolSub1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FoodRegisterationToolSub1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
