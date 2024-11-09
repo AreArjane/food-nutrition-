676c18debe551fb95839502cc023ece7e886dfae
armah9337 <97673925+AreArjane@users.noreply.github.com>
2024-11-09 12:12:35 +0100

    update

--- FoodRegisterationToolSub1/obj/Debug/net8.0/FoodRegisterationToolSub1.AssemblyInfo.cs
+++ FoodRegisterationToolSub1/obj/Debug/net8.0/FoodRegisterationToolSub1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FoodRegisterationToolSub1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff2620f8d06aa99706b039fd29e97684f05eb3e7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83f2488e6e684cd52c840f0a3d26949a22429e60")]
 [assembly: System.Reflection.AssemblyProductAttribute("FoodRegisterationToolSub1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FoodRegisterationToolSub1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
