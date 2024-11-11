9fbdcd365cef8b43971fcd3d453dcbe77483b615
Shefaa62 <s369566@oslomet.no>
2024-11-11 03:17:46 +0100

    update login

--- FoodRegisterationToolSub1/obj/Debug/net8.0/FoodRegisterationToolSub1.AssemblyInfo.cs
+++ FoodRegisterationToolSub1/obj/Debug/net8.0/FoodRegisterationToolSub1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FoodRegisterationToolSub1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8acd1972c30105cd4260e24c18c56d7c36b7534d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8e71c92c5425c256dcb05170eb1a27cbea3e23c")]
 [assembly: System.Reflection.AssemblyProductAttribute("FoodRegisterationToolSub1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FoodRegisterationToolSub1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
