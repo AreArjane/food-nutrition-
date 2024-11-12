1301338f97e5231f77d798c44295ecc9102904fb
mahsaimani <114579479+mahsaimani@users.noreply.github.com>
2024-11-12 11:47:27 +0100

    Update HomeController.cs

--- FoodRegisterationToolSub1/Controllers/HomeController.cs
+++ FoodRegisterationToolSub1/Controllers/HomeController.cs
@@ -1,7 +1,15 @@
 using Microsoft.AspNetCore.Mvc;
-
+/// <summary>
+/// Controller for håndtering av hjemmesiden til applikasjonen.
+/// </summary>
 public class HomeController : Controller
 {
+/// <summary>
+    /// Henter startsiden for applikasjonen.
+    /// </summary>
+    /// <returns>
+    /// En <see cref="IActionResult"/> som representerer visningen av startsiden.
+    /// </returns>
     [HttpGet("")]
     public IActionResult Index()
     {
