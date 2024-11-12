053d1d1d117543cf08764553851c5155462fb0d5
mahsaimani <114579479+mahsaimani@users.noreply.github.com>
2024-11-12 11:49:17 +0100

    Update med kommentar LoginController.cs

--- FoodRegisterationToolSub1/Controllers/LoginController.cs
+++ FoodRegisterationToolSub1/Controllers/LoginController.cs
@@ -1,8 +1,17 @@
 using Microsoft.AspNetCore.Mvc;
+/// <summary>
+/// Controller for håndtering av brukerinnlogging.
+/// </summary>
 [Route("Login")]
 public class LoginController : Controller
     {
         // Action to serve the login view
+        /// <summary>
+    /// Viser innloggingssiden for brukeren.
+    /// </summary>
+    /// <returns>
+    /// En <see cref="IActionResult"/> som representerer visningen av innloggingssiden.
+    /// </returns>
         [HttpGet("")]
         public IActionResult Login()
         {
