a28c6aad47f8bc13d7a14f090e401d366d0bba02
Shefaa62 <s369566@oslomet.no>
2024-11-14 18:14:27 +0100

    image

--- food_registeration_tool_sub_2/src/pages/Home/Home.js
+++ food_registeration_tool_sub_2/src/pages/Home/Home.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import foodImage from '../../assets/images/food-image.jpg';
 
 const Home = () => {
   return (
     <div className="home-container">
+      <img src={foodImage} alt="Delicious food" className="home-image" />
       <h2 className="text-center">Welcome to the Food App!</h2>
       <p className="text-center">Platform designed to help you find and share delicious recipes.</p>
       <p className="text-center">Whether you're a beginner or an experienced cook, we have something for everyone.</p>
