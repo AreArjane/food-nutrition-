a6c8d27ee4a61dd9f4df74aade9030d84e9a962f
arjan <97673925+AreArjane@users.noreply.github.com>
2024-11-27 22:36:34 +0100

    Update IEmailService.cs (#40)

--- FoodRegisterationToolSub1/Services/EmailManagement/IEmailService.cs
+++ FoodRegisterationToolSub1/Services/EmailManagement/IEmailService.cs
@@ -11,35 +11,8 @@ using System.IO;
 namespace EmailService {
 
     internal class EmailVerification {
-        private static async Task Main() {
+        
 
-            await Execute();
-        }
-
-        public static async Task Execute()  {
-
-            string base64image = "data:image/jpeg;base64," + Base64Converter.ConvertImageToBase64("wwwroot/images/email.png");
-
-            var apiKey = Env.GetString("ApiKeySenGridConfiguration");
-            var client = new SendGridClient(apiKey);
-            var from = new EmailAddress("noreplay", "Hilsen fra kollega");
-            var subject = "Sending with SendGrid is Fun";
-            var to = new EmailAddress("", "Example User");
-            
-            var htmlContent  = $@"
-            <h1>Welcome to Our Service</h1>
-            <p>This is a test email with an image:</p>
-            <img src='{base64image}' alt='Embedded Image' />
-            <p>Thank you for signing up!</p>";
-            var plaintext = "Hello";
-
-            var msg = MailHelper.CreateSingleEmail(from, to, subject,plaintext, htmlContent);
-            var response = await client.SendEmailAsync(msg);
-
-            Console.WriteLine($"Response Status Code: {response.StatusCode}");
-            Console.WriteLine($"Response Body: {await response.Body.ReadAsStringAsync()}");
-            Console.WriteLine($"Response Headers: {response.Headers}");
-        }
 
         public static async Task SendVerificationCode(string email, string verificationCode) {
 
@@ -57,10 +30,7 @@ namespace EmailService {
 
             var msg = MailHelper.CreateSingleEmail(from, to, subject, "Your verification code", htmlContent);
 
-            await client.SendEmailAsync(msg);
+            //await client.SendEmailAsync(msg);
 
             
             
-                    }
-    }
-}
