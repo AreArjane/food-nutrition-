755182b6b550e80cd71713b9bd3fc2b70c707a5b
mahsaimani <114579479+mahsaimani@users.noreply.github.com>
2024-11-29 21:55:30 +0100

    Update HomeController.cs

--- FoodRegisterationToolSub1/Controllers/HomeController.cs
+++ FoodRegisterationToolSub1/Controllers/HomeController.cs
@@ -1,10 +1,17 @@
 using EmailService;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+/// <summary>
+/// Controller responsible for handling general application requests, such as the home page.
+/// </summary>
 [Route("")]
 [ApiController]
 public class HomeController : Controller
 {
+ /// <summary>
+    /// Serves the home page of the application.
+    /// </summary>
+    /// <returns>An <see cref="IActionResult"/> that renders the home page view.</returns>
     [HttpGet("")]
     public IActionResult Index()
     {
