d91202d919fca55795d4fd7e75f7be311c5cf43a
SamiCenkci <121030749+SamiCenkci@users.noreply.github.com>
2024-12-01 16:09:46 +0100

    Update App.test.js Kommentarerene

--- food_registeration_tool_sub_2/src/App.test.js
+++ food_registeration_tool_sub_2/src/App.test.js
@@ -1,8 +1,19 @@
+// Importerer nødvendige moduler fra '@testing-library/react' for testing
 import { render, screen } from '@testing-library/react';
+
+// Importerer App-komponenten som skal testes
 import App from './App';
 
+// Definerer en test for å sjekke om en bestemt tekst finnes i App-komponenten
 test('renders learn react link', () => {
+
+  // Render App-komponenten i et testmiljø
   render(<App />);
+
+  // Søker etter et element som inneholder teksten "learn react"
+  // Teksten er ikke case-sensitiv på grunn av regex-flagget /i
   const linkElement = screen.getByText(/learn react/i);
+
+  // Forventer at elementet skal finnes i dokumentet
   expect(linkElement).toBeInTheDocument();
 });
