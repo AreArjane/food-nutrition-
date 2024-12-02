62c96f0d33be450c6d2acfb0a1c94b0eaa9d370e
arjan <97673925+AreArjane@users.noreply.github.com>
2024-12-02 01:58:53 +0100

    Update Home.js

--- food_registeration_tool_sub_2/src/pages/Home/Home.js
+++ food_registeration_tool_sub_2/src/pages/Home/Home.js
@@ -1,19 +1,30 @@
-import React from 'react'; // Import React library
-import foodImage from '../../assets/images/food-image.jpg'; // Import the food image asset
+import React from 'react';
+import foodImage from '../../assets/images/food-image.jpg';
+import foodImage1 from '../../assets/images/food-image1.jpg';
+import foodImage2 from '../../assets/images/food-image2.png';
+import foodImage3 from '../../assets/images/food-image3.jpg';
+import foodImage4 from '../../assets/images/food-image4.jpg';
 
-// Define the Home component
 const Home = () => {
   return (
     <div className="home-container">
-      <img src={foodImage} alt="Delicious food" className="home-image" />
+      {/* Gallery Section */}
+      <div className="image-gallery">
+      <img src={foodImage} alt="Delicious food" className="gallery-image" />
+        <img src={foodImage1} alt="Food 1" className="gallery-image" />
+        <img src={foodImage2} alt="Food 2" className="gallery-image" />
+        <img src={foodImage3} alt="Food 3" className="gallery-image" />
+        <img src={foodImage4} alt="Food 4" className="gallery-image" />
+      </div>
+
+      {/* Welcome Description */}
+      
       <h2 className="text-center">Welcome to the Food App!</h2>
       <p className="text-center">Platform designed to help you find and share delicious recipes.</p>
       <p className="text-center">Whether you're a beginner or an experienced cook, we have something for everyone.</p>
-       
       <p className="text-center">Sign up today to get started and share your food experiences!</p>
     </div>
   );
 };
 
-
-export default Home; // Export the Home component for use in other parts of the application
+export default Home;
